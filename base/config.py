dee11ccfa4b0e3dc71e98ed1f669f13fbb12d6b4
inshev <sergey.inshev@gmail.com>
2016-10-13 23:33:28 +0200

    fixed spelling mistakes

--- base/config.py
+++ base/config.py
@@ -1,10 +1,10 @@
 """Environment"""
 import sys
 
-# defining environment via command line argument --env=<ENV> (e.g.: --env=local)
+# defining environment via command line argument --env=<ENV> (e.g.: --env='local')
 # ENV in ['local', 'jenkins']
 
-ENV = 'local'  # BY DEFAULT the environent is set to local
+ENV = 'local'  # BY DEFAULT the environment is set to local
 
 opts = {}
 for x in sys.argv:
@@ -13,7 +13,7 @@ for x in sys.argv:
         try:
             ENV = opts['--env'].lower()
         except (KeyError, IndexError) as e:
-            ENV = 'qa'
+            ENV = 'jenkins'
 
 """Web URLs"""
 LOGIN_URL = 'https://core.futuresimple.com/sales/users/login'
